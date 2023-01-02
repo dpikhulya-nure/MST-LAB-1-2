1e57f204eff62d8b2bea5064a1fcdb5fc42d8cb3
Dmitry Pikhulya <dmytro.pikhulia@nure.ua>
2023-01-02 15:05:39 +0200

    Lab 1 first version

---
+++ src/BookBuyerAgent.java
@@ -0,0 +1,149 @@
+import jade.core.AID;
+import jade.core.Agent;
+import jade.core.behaviours.Behaviour;
+import jade.core.behaviours.TickerBehaviour;
+import jade.domain.FIPAException;
+import jade.lang.acl.ACLMessage;
+import jade.lang.acl.MessageTemplate;
+
+public class BookBuyerAgent extends Agent {
+
+    private String targetBookTitle;
+    private AID[] sellerAgents;
+
+    @Override
+    protected void setup() {
+        System.out.println("Доброго дня! Агент з покупки книг розпочав роботу: " + getAID().getName());
+
+        Object[] args = getArguments();
+        if (args != null && args.length > 0) {
+            targetBookTitle = (String) args[0];
+            System.out.println("Намагаюсь купити: " + targetBookTitle);
+
+            addBehaviour(new TickerBehaviour(this, 5000) {
+                protected void onTick() {
+					System.out.println("Trying to buy "+targetBookTitle);
+					// Update the list of seller agents
+					DFAgentDescription template = new DFAgentDescription();
+					ServiceDescription sd = new ServiceDescription();
+					sd.setType("book-selling");
+					template.addServices(sd);
+					try {
+						DFAgentDescription[] result = DFService.search(myAgent, template); 
+						System.out.println("Found the following seller agents:");
+						sellerAgents = new AID[result.length];
+						for (int i = 0; i < result.length; ++i) {
+							sellerAgents[i] = result[i].getName();
+							System.out.println(sellerAgents[i].getName());
+						}
+					}
+					catch (FIPAException fe) {
+						fe.printStackTrace();
+					}
+                    // Perform the request
+					myAgent.addBehaviour(new RequestPerformer());
+                }
+            });
+        } else {
+            System.out.println("Параметр назви книги не заданий");
+            doDelete();
+        }
+    }
+
+    protected void takeDown() {
+        System.out.println("Агент з покупки книг завершує свою роботу: " + getAID().getName());
+    }
+
+    private class RequestPerformer extends Behaviour {
+        private AID bestSeller;
+        private int bestPrice;
+        private int repliesCnt = 0;
+        private MessageTemplate mt;
+        private int step = 0;
+    
+		public void action() {
+			switch (step) {
+			case 0:
+				// Send the cfp to all sellers
+				ACLMessage cfp = new ACLMessage(ACLMessage.CFP);
+				for (int i = 0; i < sellerAgents.length; ++i) {
+					cfp.addReceiver(sellerAgents[i]);
+				} 
+				cfp.setContent(targetBookTitle);
+				cfp.setConversationId("book-trade");
+				cfp.setReplyWith("cfp"+System.currentTimeMillis()); // Unique value
+				myAgent.send(cfp);
+				// Prepare the template to get proposals
+				mt = MessageTemplate.and(MessageTemplate.MatchConversationId("book-trade"),
+						MessageTemplate.MatchInReplyTo(cfp.getReplyWith()));
+				step = 1;
+				break;
+			case 1:
+				// Receive all proposals/refusals from seller agents
+				ACLMessage reply = myAgent.receive(mt);
+				if (reply != null) {
+					// Reply received
+					if (reply.getPerformative() == ACLMessage.PROPOSE) {
+						// This is an offer 
+						int price = Integer.parseInt(reply.getContent());
+						if (bestSeller == null || price < bestPrice) {
+							// This is the best offer at present
+							bestPrice = price;
+							bestSeller = reply.getSender();
+						}
+					}
+					repliesCnt++;
+					if (repliesCnt >= sellerAgents.length) {
+						// We received all replies
+						step = 2; 
+					}
+				}
+				else {
+					block();
+				}
+				break;
+			case 2:
+				// Send the purchase order to the seller that provided the best offer
+				ACLMessage order = new ACLMessage(ACLMessage.ACCEPT_PROPOSAL);
+				order.addReceiver(bestSeller);
+				order.setContent(targetBookTitle);
+				order.setConversationId("book-trade");
+				order.setReplyWith("order"+System.currentTimeMillis());
+				myAgent.send(order);
+				// Prepare the template to get the purchase order reply
+				mt = MessageTemplate.and(MessageTemplate.MatchConversationId("book-trade"),
+						MessageTemplate.MatchInReplyTo(order.getReplyWith()));
+				step = 3;
+				break;
+			case 3:      
+				// Receive the purchase order reply
+				reply = myAgent.receive(mt);
+				if (reply != null) {
+					// Purchase order reply received
+					if (reply.getPerformative() == ACLMessage.INFORM) {
+						// Purchase successful. We can terminate
+						System.out.println(targetBookTitle+" successfully purchased from agent "+reply.getSender().getName());
+						System.out.println("Price = "+bestPrice);
+						myAgent.doDelete();
+					}
+					else {
+						System.out.println("Attempt failed: requested book already sold.");
+					}
+
+					step = 4;
+				}
+				else {
+					block();
+				}
+				break;
+			}        
+        }
+
+        public boolean done() {
+            if (step == 2 && bestSeller == null) {
+                System.out.println("Attempt failed: " + targetBookTitle + " not available for sale");
+            }
+            return ((step == 2 && bestSeller == null) || step == 4);
+        }
+    }
+}
---
+++ src/BookSellerAgent.java
@@ -0,0 +1,114 @@
+import java.util.Hashtable;
+import jade.core.Agent;
+import jade.core.behaviours.CyclicBehaviour;
+import jade.core.behaviours.OneShotBehaviour;
+import jade.domain.DFService;
+import jade.domain.FIPAException;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
+import jade.lang.acl.ACLMessage;
+import jade.lang.acl.MessageTemplate;
+
+public class BookSellerAgent extends Agent {
+    private Hashtable<String, Integer> catalogue;
+    private BookSellerGui myGui;
+
+    protected void setup() {
+        System.out.println("Агент з продажу розпочинає роботу: " + getAID().getName());
+
+        catalogue = new Hashtable();
+
+        myGui = new BookSellerGui(this);
+        myGui.show();
+
+        // Register the book-selling service in the yellow pages
+		DFAgentDescription dfd = new DFAgentDescription();
+		dfd.setName(getAID());
+		ServiceDescription sd = new ServiceDescription();
+		sd.setType("book-selling");
+		sd.setName("JADE-book-trading");
+		dfd.addServices(sd);
+		try {
+			DFService.register(this, dfd);
+		}
+		catch (FIPAException fe) {
+			fe.printStackTrace();
+		}
+        addBehaviour(new OfferRequestsServer());
+        addBehavior(new PurchaseOrdersServer());
+    }
+
+    protected void takeDown() {
+		try {
+			DFService.deregister(this);
+		}
+		catch (FIPAException fe) {
+			fe.printStackTrace();
+		}
+		// Close the GUI
+		myGui.dispose();
+        System.out.println("Агент з продажу завершує роботу: " + getAID().getName());
+    }
+
+    public void updateCatalogue(String title, int price) {
+        addBehaviour(new OneShotBehaviour() {
+            public void action() {
+                catalogue.put(title, price);
+            }
+        });
+    }
+
+    public class OfferRequestsServer extends CyclicBehaviour {
+        @Override
+        public void action() {
+            MessageTemplate mt = MessageTemplate.MatchPerformative(ACLMessage.CFP);
+            ACLMessage msg = myAgent.receive(mt);
+            if (msg != null) {
+                String title = msg.getContent();
+                ACLMessage reply = msg.createReply();
+
+                Integer price = catalogue.get(title);
+                if (price != null) {
+                    reply.setPerformative(ACLMessage.PROPOSE);
+					reply.setContent(price.toString());
+                } else {
+                    reply.setPerformative(ACLMessage.REFUSE);
+                    reply.setContent("not-available");
+                }
+                myAgent.send(reply);
+                
+            } else {
+                block();
+            }
+        }
+        
+    }
+
+    private class PurchaseOrdersServer extends CyclicBehaviour {
+		public void action() {
+			MessageTemplate mt = MessageTemplate.MatchPerformative(ACLMessage.ACCEPT_PROPOSAL);
+			ACLMessage msg = myAgent.receive(mt);
+			if (msg != null) {
+				// ACCEPT_PROPOSAL Message received. Process it
+				String title = msg.getContent();
+				ACLMessage reply = msg.createReply();
+
+				Integer price = (Integer) catalogue.remove(title);
+				if (price != null) {
+					reply.setPerformative(ACLMessage.INFORM);
+					System.out.println(title+" sold to agent "+msg.getSender().getName());
+				}
+				else {
+					// The requested book has been sold to another buyer in the meanwhile .
+					reply.setPerformative(ACLMessage.FAILURE);
+					reply.setContent("not-available");
+				}
+				myAgent.send(reply);
+			}
+			else {
+				block();
+			}
+		}
+	}
+    
+}
