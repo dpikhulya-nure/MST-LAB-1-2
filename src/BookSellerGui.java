006a547cebcc6f4e3f75f566421bf5bc104b53bc
Dmitry Pikhulya <dmytro.pikhulia@nure.ua>
2023-01-03 16:29:02 +0200

    Make some corrections

--- src/BookSellerGui.java
+++ src/BookSellerGui.java
@@ -21,8 +21,6 @@ Free Software Foundation, Inc., 59 Temple Place - Suite 330,
 Boston, MA  02111-1307, USA.
 *****************************************************************/
 
-import jade.core.AID;
-
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
