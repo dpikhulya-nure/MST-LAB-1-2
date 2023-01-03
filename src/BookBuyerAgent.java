a4969b70eb000d3f38e8016f89b8739bea18b457
Dmitry Pikhulya <dmytro.pikhulia@nure.ua>
2023-01-03 15:14:59 +0200

    Fix some compilation errors

--- src/BookBuyerAgent.java
+++ src/BookBuyerAgent.java
@@ -2,7 +2,10 @@ import jade.core.AID;
 import jade.core.Agent;
 import jade.core.behaviours.Behaviour;
 import jade.core.behaviours.TickerBehaviour;
+import jade.domain.DFService;
 import jade.domain.FIPAException;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
 import jade.lang.acl.ACLMessage;
 import jade.lang.acl.MessageTemplate;
 
